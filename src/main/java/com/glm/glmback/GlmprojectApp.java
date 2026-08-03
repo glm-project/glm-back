e8e706a3916c82210f087c00fe67443899754cb2
loic <loicjurado@free.fr>
2026-08-03 08:39:43 +0200

    Apply module: spring-boot

---
+++ src/main/java/com/glm/glmback/GlmprojectApp.java
@@ -0,0 +1,23 @@
+package com.glm.glmback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import com.glm.glmback.shared.generation.domain.ExcludeFromGeneratedCodeCoverage;
+
+@SpringBootApplication
+@ExcludeFromGeneratedCodeCoverage(reason = "Not testing logs")
+public class GlmprojectApp {
+
+  private static final Logger log = LoggerFactory.getLogger(GlmprojectApp.class);
+
+  public static void main(String[] args) {
+    Environment env = SpringApplication.run(GlmprojectApp.class, args).getEnvironment();
+
+    if (log.isInfoEnabled()) {
+      log.info(ApplicationStartupTraces.of(env));
+    }
+  }
+}
