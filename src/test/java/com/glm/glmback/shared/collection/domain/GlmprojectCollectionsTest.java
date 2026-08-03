34b970472905d51d80296c3b5f23c35318a61d3d
loic <loicjurado@free.fr>
2026-08-03 08:39:42 +0200

    Apply module: java-base

---
+++ src/test/java/com/glm/glmback/shared/collection/domain/GlmprojectCollectionsTest.java
@@ -0,0 +1,115 @@
+package com.glm.glmback.shared.collection.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import com.glm.glmback.UnitTest;
+
+@UnitTest
+class GlmprojectCollectionsTest {
+
+  @Nested
+  @DisplayName("Collections")
+  class GlmprojectCollectionsCollectionsTest {
+
+    @Test
+    void shouldGetEmptyImmutableCollectionFromNullCollection() {
+      Collection<Object> input = null;
+      Collection<Object> collection = GlmprojectCollections.immutable(input);
+
+      assertThat(collection).isEmpty();
+      assertThatThrownBy(collection::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableCollectionFromMutableCollection() {
+      Collection<String> input = new ArrayList<>();
+      input.add("value");
+      Collection<String> collection = GlmprojectCollections.immutable(input);
+
+      assertThat(collection).containsExactly("value");
+      assertThatThrownBy(collection::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("Set")
+  class GlmprojectCollectionsSetTest {
+
+    @Test
+    void shouldGetEmptyImmutableCollectionFromNullCollection() {
+      Set<Object> input = null;
+      Set<Object> set = GlmprojectCollections.immutable(input);
+
+      assertThat(set).isEmpty();
+      assertThatThrownBy(set::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableCollectionFromMutableCollection() {
+      Set<String> input = new HashSet<>();
+      input.add("value");
+      Set<String> set = GlmprojectCollections.immutable(input);
+
+      assertThat(set).containsExactly("value");
+      assertThatThrownBy(set::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("List")
+  class GlmprojectCollectionsListTest {
+
+    @Test
+    void shouldGetEmptyImmutableCollectionFromNullCollection() {
+      List<Object> input = null;
+      List<Object> list = GlmprojectCollections.immutable(input);
+
+      assertThat(list).isEmpty();
+      assertThatThrownBy(list::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableCollectionFromMutableCollection() {
+      List<String> input = new ArrayList<>();
+      input.add("value");
+      List<String> list = GlmprojectCollections.immutable(input);
+
+      assertThat(list).containsExactly("value");
+      assertThatThrownBy(list::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("Map")
+  class GlmprojectMapTest {
+
+    @Test
+    void shouldGetEmptyImmutableMapFromNullMap() {
+      Map<Object, Object> input = null;
+      Map<Object, Object> map = GlmprojectCollections.immutable(input);
+
+      assertThat(map).isEmpty();
+      assertThatThrownBy(map::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableMapFromMutableMap() {
+      Map<String, String> input = new HashMap<>();
+      input.put("key", "value");
+      Map<String, String> map = GlmprojectCollections.immutable(input);
+
+      assertThat(map).containsExactly(Map.entry("key", "value"));
+      assertThatThrownBy(map::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+}
